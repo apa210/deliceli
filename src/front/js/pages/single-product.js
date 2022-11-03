bae8bd1f9eba892b5cbed547d17db37ca14dabef
Nosoir <noeliairace13@gmail.com>
2022-11-03 17:58:31 +0000

    Alerta de exito en registro

--- src/front/js/pages/single-product.js
+++ src/front/js/pages/single-product.js
@@ -23,6 +23,7 @@ export const SingleProduct = () => {
   };
 
 useEffect(() => {
+  window.scrollTo(0, 0);
   actions.getProduct(params?.id);
 }, [params?.id]);
 
@@ -50,81 +51,10 @@ kitchen_api()
               <div className="card border-0 mb-5 mt-5">
                 <div className="row g-0">
                   <div className="col-md-7">
-                    <div
-                      id="carouselExampleIndicators"
-                      className="carousel slide"
-                      data-bs-ride="true"
-                    >
-                      <div className="carousel-indicators">
-                        <button
-                          type="button"
-                          data-bs-target="#carouselExampleIndicators"
-                          data-bs-slide-to="0"
-                          className="active"
-                          aria-current="true"
-                          aria-label="Slide 1"
-                        ></button>
-                        <button
-                          type="button"
-                          data-bs-target="#carouselExampleIndicators"
-                          data-bs-slide-to="1"
-                          aria-label="Slide 2"
-                        ></button>
-                        <button
-                          type="button"
-                          data-bs-target="#carouselExampleIndicators"
-                          data-bs-slide-to="2"
-                          aria-label="Slide 3"
-                        ></button>
-                      </div>
-                      <div className="carousel-inner">
-                        <div className="carousel-item active">
-                          <img
-                            src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-                            className="d-block w-100"
-                            alt="..."
-                          />
-                        </div>
-                        <div className="carousel-item">
-                          <img
-                            src="https://images.unsplash.com/photo-1625248442085-10a1a2563dd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                            className="d-block w-100"
-                            alt="..."
-                          />
-                        </div>
-                        <div className="carousel-item">
-                          <img
-                            src="https://images.unsplash.com/photo-1546069901-04dcb46a5e26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80"
-                            className="d-block w-100"
-                            alt="..."
-                          />
-                        </div>
-                      </div>
-                      <button
-                        className="carousel-control-prev"
-                        type="button"
-                        data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="prev"
-                      >
-                        <span
-                          className="carousel-control-prev-icon"
-                          aria-hidden="true"
-                        ></span>
-                        <span className="visually-hidden">Previous</span>
-                      </button>
-                      <button
-                        className="carousel-control-next"
-                        type="button"
-                        data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="next"
-                      >
-                        <span
-                          className="carousel-control-next-icon"
-                          aria-hidden="true"
-                        ></span>
-                        <span className="visually-hidden">Next</span>
-                      </button>
-                    </div>
+                    <img
+                      src={store?.product?.foto}
+                      className="img-fluid rounded-start"
+                    />
                   </div>
                   <div className="col-md-5 p-3">
                     <div className="card-body">
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -3,8 +3,9 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       api_url:
-        "https://3001-apa210-deliceli-cfusbv71ezu.ws-us73.gitpod.io/api/",
+        "https://3001-apa210-deliceli-cfusbv71ezu.ws-us74.gitpod.io/api/",
       auth: false,
+      val: false,
       profile: {},
       AllProducts: [],
       AllKitchens: [],
@@ -117,15 +118,19 @@ const getState = ({ getStore, getActions, setStore }) => {
           });
 
           if (response.status === 200) {
-            alert("Registrado con exito");
+            setStore({
+              val: true,
+            });
           } else {
             setStore({
               auth: false,
             });
+            setStore({
+              val: false,
+            });
           }
         } catch (error) {
           if (error.code === "ERR_BAD_REQUEST") {
-            alert(error.response?.data?.message);
             setStore({
               auth: false,
             });
