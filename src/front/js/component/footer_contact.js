69e43f1242c5ac8b41fb9f09466750166b6087a9
analiacorcino <analia.corcino@gmail.com>
2022-11-08 17:46:43 +0000

    ft-001-ajustes-checkout-2

--- src/front/js/component/footer_contact.js
+++ src/front/js/component/footer_contact.js
@@ -9,8 +9,7 @@ const Footer_contact = () => {
           <div className="row">
             <div className="col-lg-8">
               <h2 className="text-light mt-lg-4 px-lg-5 mx-lg-5 p-lg-3">
-                Buscamos facilitar la vida de las personas con Enfermedad
-                Celíaca, intolerancia al gluten o alergias.
+                Buscamos facilitar la vida de las personas con celiaquía, intolerancia al gluten o alergias.
               </h2>
             </div>
             <div className="col">
