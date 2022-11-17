e36e06f7b06d28116b4c4c65492b72685afbd9a6
analiacorcino <analia.corcino@gmail.com>
2022-11-17 21:39:00 +0000

    ft-001-cambios-check-final

--- src/front/js/pages/checkout.js
+++ src/front/js/pages/checkout.js
@@ -114,144 +114,131 @@ export const Checkout = () => {
 
             {/* Detalles de contacto */}
             <div className="col">
-              <h4 className="mb-4 mt-5">Envío y datos de pago</h4>
+              <h4 className="mb-4 mt-5">Formas de pago</h4>
 
-              <div className="accordion" id="accordionExample">
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="headingOne">
-                    <button
-                      className="accordion-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseOne"
-                      aria-expanded="true"
-                      aria-controls="collapseOne"
+              {/* _________ */}
+
+              <div className="container mt-3">
+                <div className="accordion" id="accordionPanelsStayOpenExample">
+                  <div className="accordion-item">
+                    <div
+                      className="accordion-header"
+                      id="panelsStayOpen-headingOne"
                     >
-                      Pago con tarjetas mediante MERCADO PAGO
-                    </button>
-                  </h2>
-                  <div
-                    id="collapseOne"
-                    className="accordion-collapse collapse show"
-                    aria-labelledby="headingOne"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="accordion-body">
-                      <img src="https://sintropia.com.uy/desarrollo/mpago.png" />
-
-                      <br />
-                      <br />
-                      <div className="form-check  alert alert-secondary">
+                      <button
+                        className="accordion-button"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#panelsStayOpen-collapseOne"
+                        aria-expanded="true"
+                        aria-controls="panelsStayOpen-collapseOne"
+                        id="accordion1btn"
+                      >
                         <input
-                          className="form-check-input"
+                          className="form-check-input chkAll me-2"
                           type="checkbox"
                           value=""
-                          id="flexCheckDefault"
+                          id="chkAccordion1All"
                         />
-                        <label
-                          className="form-check-label "
-                          for="flexCheckDefault"
-                        >
-                          SELECCIONAR MERCADO PAGO
-                        </label>
-                      </div>
-
-
-
+                        Pago con tarjetas mediante MERCADO PAGO
+                      </button>
                     </div>
-                  </div>
-                </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="headingTwo">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseTwo"
-                      aria-expanded="false"
-                      aria-controls="collapseTwo"
+                    <div
+                      id="panelsStayOpen-collapseOne"
+                      className="accordion-collapse collapse show"
+                      aria-labelledby="panelsStayOpen-headingOne"
                     >
-                      Pago en efectivo
-                    </button>
-                  </h2>
-                  <div
-                    id="collapseTwo"
-                    className="accordion-collapse collapse"
-                    aria-labelledby="headingTwo"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="accordion-body">
-                      PagoEfectivo es el medio de pago para comprar por internet
-                      y pagar sin tarjeta. <br />
-                      <br /> Realiza depósitos en efectivo en agencias o agentes
-                      o pagá tu entrega al repartidor.
-                      <br />
-                      <br />
-                      <div className="form-check  alert alert-secondary">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          value=""
-                          id="flexCheckDefault"
-                        />
-                        <label
-                          className="form-check-label "
-                          for="flexCheckDefault"
-                        >
-                          SELECCIONAR PAGO EN EFECTIVO
-                        </label>
+                      <div className="accordion-body ms-3">
+                        <img src="https://sintropia.com.uy/desarrollo/mpago.png" />
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="headingThree">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseThree"
-                      aria-expanded="false"
-                      aria-controls="collapseThree"
+                  <div className="accordion-item">
+                    <h2
+                      className="accordion-header"
+                      id="panelsStayOpen-headingTwo"
                     >
-                      Transferencia bancaria directa
-                    </button>
-                  </h2>
-                  <div
-                    id="collapseThree"
-                    className="accordion-collapse collapse"
-                    aria-labelledby="headingThree"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="accordion-body">
-                      Realiza tu pago directamente en nuestra cuenta bancaria.
-                      Tienes tiempo de hacerlo hasta 30 minutos después del
-                      pedido. <br />
-                      <br />
-                      Por favor, usa el número del pedido como referencia de
-                      pago y envía foto del comprobante de depósito a nuestro
-                      whatsapp. <br /> <br />
-                      Tu pedido no se procesará hasta que se haya recibido el
-                      importe en nuestra cuenta.<br />
-                      <br />
-                      <div className="form-check  alert alert-secondary">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          value=""
-                          id="flexCheckDefault"
-                        />
-                        <label
-                          className="form-check-label "
-                          for="flexCheckDefault"
-                        >
-                          SELECCIONAR PAGO TRANSFERENCIA BANCARIA
-                        </label>
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#panelsStayOpen-collapseTwo"
+                        aria-expanded="false"
+                        aria-controls="panelsStayOpen-collapseTwo"
+                      >
+                        <div className="form-check mt-1">
+                          <input
+                            className="form-check-input chkAll"
+                            type="checkbox"
+                            value=""
+                            id="chkAccordion2All"
+                          />
+                        </div>
+                        Pago Efectivo
+                      </button>
+                    </h2>
+                    <div
+                      id="panelsStayOpen-collapseTwo"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="panelsStayOpen-headingTwo"
+                    >
+                      <div className="accordion-body ms-3">
+                        Pago Efectivo es el medio de pago para comprar por
+                        internet y pagar sin tarjeta. <br />
+                        <br /> Realiza depósitos en efectivo en agencias o
+                        agentes o pagá tu entrega al repartidor.
+                        <br />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="accordion-item">
+                    <h2
+                      className="accordion-header"
+                      id="panelsStayOpen-headingThree"
+                    >
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#panelsStayOpen-collapseThree"
+                        aria-expanded="false"
+                        aria-controls="panelsStayOpen-collapseThree"
+                      >
+                        <div className="form-check mt-1">
+                          <input
+                            className="form-check-input chkAll"
+                            type="checkbox"
+                            value=""
+                            id="chkAccordion3All"
+                          />
+                        </div>
+                        Transferencia bancaria directa
+                      </button>
+                    </h2>
+                    <div
+                      id="panelsStayOpen-collapseThree"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="panelsStayOpen-headingThree"
+                    >
+                      <div className="accordion-body ms-3">
+                        Realiza tu pago directamente en nuestra cuenta bancaria.
+                        Tienes tiempo de hacerlo hasta 30 minutos después del
+                        pedido. <br />
+                        <br />
+                        Por favor, usa el número del pedido como referencia de
+                        pago y envía foto del comprobante de depósito a nuestro
+                        whatsapp. <br /> <br />
+                        Tu pedido no se procesará hasta que se haya recibido el
+                        importe en nuestra cuenta.
+                        <br />
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
+
+              {/* _________ */}
+
               <button
                 type="button"
                 className="btn btn-primary btn-lg mb-3 mt-4"
@@ -261,7 +248,7 @@ export const Checkout = () => {
             </div>
             {/* Detalles de contacto */}
           </div>
-        </div> 
+        </div>
       </section>
 
       <div className="gap-3 d-md-flex justify-content-md-end text-center mb-5"></div>
